c3487aa7c261a0265e5d04683abec0680a9084e0
Kendy Alba <kendyalba907@gmail.com>
2026-09-01 11:51:33 -0600

    CREATE  Unidad2

---
+++ Unidad2/01_condiciones.py
@@ -0,0 +1,7 @@
+print(bool(1))
+print(bool(0))
+print(bool(-1))
+print(bool(100000))
+print(bool(10.5))
+print(bool("A"))
+#todo valor diferente de 0 buleano dara true 
---
+++ Unidad2/02_operadores.py
@@ -0,0 +1,6 @@
+operador=input("Ingrese un operador (+, -, *, /): ")
+o1 =input ("introduce el operador :")
+o2 =input ("introduce el operador :")
+resultado=o1+operador+o2
+resultado=eval(resultado)
+print(resultado)
